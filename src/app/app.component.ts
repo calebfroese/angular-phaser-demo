b1f92d9d26441a5cb90fe8ed136fee2d2343e387
Caleb Froese <caleb.froese@gmail.com>
2018-01-20 13:12:32 +1030

    Add phaser.io

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as Phaser from 'phaser-ce/build/custom/phaser-split';
 
 @Component({
   selector: 'app-root',
@@ -6,57 +7,37 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  ctx: CanvasRenderingContext2D;
-  canvas: HTMLCanvasElement;
-  player: any = { x: 20, y: 0 };
-
   @ViewChild('page') page: ElementRef;
   @ViewChild('canvas') canvasEl: ElementRef;
   @ViewChild('title') title: ElementRef;
   @ViewChild('subtitle') subtitle: ElementRef;
 
+  game: Phaser.Game;
+
   ngOnInit() {
-    // Setup the canvas
-    this.canvas = this.canvasEl.nativeElement;
-    this.canvas.width = this.page.nativeElement.offsetWidth;
-    this.canvas.height = this.page.nativeElement.offsetHeight;
-    this.ctx = this.canvas.getContext('2d');
-
-    this.redraw();
-    const FPS = 30;
-    setInterval(() => {
-      this.redraw();
-    }, 1000 / FPS);
+    const { offsetWidth, offsetHeight } = this.page.nativeElement;
+    this.game = new Phaser.Game(
+      offsetWidth,
+      offsetHeight,
+      Phaser.AUTO,
+      'content',
+      {
+        preload: this.preload,
+        create: this.create,
+      },
+    );
   }
 
-  drawRect(ctx: CanvasRenderingContext2D, element: HTMLElement) {
-    const x = element.offsetLeft;
-    const y = element.offsetTop;
-    const width = element.offsetWidth;
-    const height = element.offsetHeight;
-    ctx.beginPath();
-    ctx.rect(x, y, width, height);
-    ctx.stroke();
-    ctx.closePath();
+  preload() {
+    this.game.load.image('logo', 'favicon.ico');
   }
 
-  drawPlayer(ctx: CanvasRenderingContext2D) {
-    this.player.y += 1;
-    ctx.beginPath();
-    ctx.rect(this.player.x, this.player.y, 20, 50);
-    ctx.stroke();
-    ctx.closePath();
-  }
-
-  redraw() {
-    const { width, height } = this.canvas;
-    this.ctx.clearRect(0, 0, width, height);
-
-    // Outline elements
-    const title: HTMLHeadElement = this.title.nativeElement;
-    const subtitle: HTMLHeadElement = this.subtitle.nativeElement;
-    this.drawRect(this.ctx, title);
-    this.drawRect(this.ctx, subtitle);
-    this.drawPlayer(this.ctx);
+  create() {
+    var logo = this.game.add.sprite(
+      this.game.world.centerX,
+      this.game.world.centerY,
+      'logo',
+    );
+    logo.anchor.setTo(0.5, 0.5);
   }
 }
