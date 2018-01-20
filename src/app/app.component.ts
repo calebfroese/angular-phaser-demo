cec8d8a63163966b4d5a9da38c777f893f9fb650
Caleb Froese <caleb.froese@gmail.com>
2018-01-20 13:39:22 +1030

    Implement phaser.io getting started tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import * as Phaser from 'phaser-ce/build/custom/phaser-split';
+import { AUTO, Game, Physics } from 'phaser-ce/build/custom/phaser-split';
 
 @Component({
   selector: 'app-root',
@@ -12,32 +12,69 @@ export class AppComponent implements OnInit {
   @ViewChild('title') title: ElementRef;
   @ViewChild('subtitle') subtitle: ElementRef;
 
-  game: Phaser.Game;
+  game: Game;
+  platforms: any;
+  player: any;
 
   ngOnInit() {
     const { offsetWidth, offsetHeight } = this.page.nativeElement;
-    this.game = new Phaser.Game(
-      offsetWidth,
-      offsetHeight,
-      Phaser.AUTO,
-      'content',
-      {
-        preload: this.preload,
-        create: this.create,
-      },
-    );
+    this.game = new Game(offsetWidth, offsetHeight, AUTO, 'content', {
+      preload: this.preload,
+      create: this.create,
+      update: this.update,
+    });
   }
 
   preload() {
-    this.game.load.image('logo', 'favicon.ico');
+    this.game.load.image('player', 'favicon.ico');
   }
 
   create() {
-    var logo = this.game.add.sprite(
-      this.game.world.centerX,
-      this.game.world.centerY,
-      'logo',
+    this.game.physics.startSystem(Physics.ARCADE);
+
+    const background = this.game.add.sprite(0, 0, 'background');
+    // Platforms
+    this.platforms = this.game.add.group();
+    this.platforms.enableBody = true;
+    const ground = this.platforms.create(50, this.game.world.height, 'ground');
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
+    //  Now let's create two ledges
+    var ledge = this.platforms.create(400, 400, 'ground');
+    ledge.body.immovable = true;
+    ledge = this.platforms.create(-150, 250, 'ground');
+    ledge.body.immovable = true;
+    // Plater
+    this.player = this.game.add.sprite(32, 150, 'player');
+    this.game.physics.arcade.enable(this.player);
+    this.player.body.gravity.y = 2000;
+    this.player.body.collideWorldBounds = true;
+    this.game.stage.backgroundColor = 0xeeeeee;
+  }
+
+  update() {
+    //  Collide the player with the platforms
+    const hitPlatform = this.game.physics.arcade.collide(
+      this.player,
+      this.platforms,
     );
-    logo.anchor.setTo(0.5, 0.5);
+    const cursors = this.game.input.keyboard.createCursorKeys();
+    this.player.body.velocity.x = 0;
+    if (cursors.left.isDown) {
+      this.player.body.velocity.x = -150;
+    } else if (cursors.right.isDown) {
+      this.player.body.velocity.x = 150;
+    } else {
+      this.player.animations.stop();
+      this.player.frame = 4;
+    }
+
+    //  Allow the player to jump if they are touching the ground.
+    const isOnGround =
+      this.player.y >= this.game.world.height - this.player.body.height;
+    const isOnPlatform = this.player.body.touching.down && hitPlatform;
+    if (cursors.up.isDown && (isOnGround || isOnPlatform)) {
+      this.player.body.velocity.y = -1000;
+    }
   }
 }
